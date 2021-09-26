1c8d4a57a149c04f1fb8367583fb46c27874f728
byanca <byanca.info@gmail.com>
2021-09-26 13:12:51 -0300

    mudanças de arquivos dentro das pastas

--- arquivos/segundo.java
+++
@@ -1,9 +0,0 @@
-public class segundo {
-    public static void main(String[] args) {
-    Display display = getWindowManager().getDefaultDisplay();
-    Point size = new Point();
-    display.getSize(size);
-    int width = size.x;
-    int height = size.y;
-    }
-}
